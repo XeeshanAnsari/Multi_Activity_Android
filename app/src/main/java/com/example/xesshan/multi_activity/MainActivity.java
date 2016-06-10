dc80e38c59012c879f6aa9935eb86294ef35e69b
zeeshan <zeeshanansari38244@gmail.com>
2016-06-09 17:57:15 -0700

    conect second activity

---
+++ app/src/main/java/com/example/xesshan/multi_activity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.xesshan.multi_activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static Button button_sbm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OnCLickBUttonListner();
+    }
+    public void OnCLickBUttonListner(){
+        button_sbm = (Button)findViewById(R.id.button);
+        button_sbm.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.example.xesshan.multi_activity.SecondActivity");
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
